7518ff9b214814741e8d826c5f1a7dbf0b46cb3e
nian0114 <268078545@qq.com>
2017-04-14 19:53:58 +0800

    新增 按钮监听事件，方便日后填充，使用Log.d测试是否正常

--- app/src/main/java/com/xbuyt/sudoku/MainActivity.java
+++ app/src/main/java/com/xbuyt/sudoku/MainActivity.java
@@ -2,12 +2,46 @@ package com.xbuyt.sudoku;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button mOnlineMode, mOfflineMode, mFAQ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mOnlineMode = (Button) findViewById(R.id.online_mode);
+        mOfflineMode = (Button) findViewById(R.id.offline_mode);
+        mFAQ = (Button) findViewById(R.id.btn_faq);
+
+        mOnlineMode.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View view) {
+                                               Log.d("Sudoku", "Online");
+                                           }
+                                       }
+
+        );
+
+        mOfflineMode.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                Log.d("Sudoku", "Offline");
+                                            }
+                                        }
+
+        );
+
+        mFAQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Sudoku", "mFaq");
+            }
+        });
     }
 }

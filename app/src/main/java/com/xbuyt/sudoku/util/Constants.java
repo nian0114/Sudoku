6b873506d77b34a9f8dd1257e07a347fb4053579
nian0114 <268078545@qq.com>
2017-04-17 03:27:22 +0800

    修复 至少17个才能解出数独(这个地方后期一定会删除)

--- app/src/main/java/com/xbuyt/sudoku/util/Constants.java
+++ app/src/main/java/com/xbuyt/sudoku/util/Constants.java
@@ -1,10 +1,10 @@
 package com.xbuyt.sudoku.util;
 
 public class Constants {
-    //根据难易程度不同，显示不同数量的数独
+    //根据难易程度不同，显示不同数量的数独，这个地方是个傻逼的决定，后面一定会删了！！！
     public static final int EASY_LEVEL_CELL_NUMBER = 40;
-    public static final int MEDIUM_LEVEL_CELL_NUMBER = 20;
-    public static final int HARD_LEVEL_CELL_NUMBER = 10;
+    public static final int MEDIUM_LEVEL_CELL_NUMBER = 30;
+    public static final int HARD_LEVEL_CELL_NUMBER = 20;
 
     public static final String EASY_LEVEL_TEXT = "Level: easy";
     public static final String MEDIUM_LEVEL_TEXT = "Level: medium";

411687801d7ed14a8db9d4eb2fc760b658477138
nian0114 <268078545@qq.com>
2017-04-18 01:17:34 +0800

    新增 模式选择窗口

---
+++ app/src/main/java/com/xbuyt/sudoku/activities/SwitchActivity.java
@@ -0,0 +1,11 @@
+package com.xbuyt.sudoku.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nian on 17/4/15.
+ */
+
+public class SwitchActivity extends AppCompatActivity {
+
+}

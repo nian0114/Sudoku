62556459e8e6d79aeef1c749ab7f4761505d9734
nian0114 <268078545@qq.com>
2017-04-18 01:25:15 +0800

    新增 相关按钮的监听

--- app/src/main/java/com/xbuyt/sudoku/activities/SwitchActivity.java
+++ app/src/main/java/com/xbuyt/sudoku/activities/SwitchActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
 import android.widget.TextView;
 
 import com.xbuyt.sudoku.R;
@@ -14,7 +16,9 @@ import com.xbuyt.sudoku.R;
 
 public class SwitchActivity extends AppCompatActivity {
     private TextView mProfileName;
-
+    private CardView mPracticeMode;
+    private CardView mNormalMode;
+    private CardView mOnlineMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +26,14 @@ public class SwitchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_switch);
 
         Intent intent = getIntent();
-        Boolean online_mode = intent.getBooleanExtra("online", false);
+        Boolean boolOnline = intent.getBooleanExtra("online", false);
 
         mProfileName = ((TextView) findViewById(R.id.profile_name));
+        mPracticeMode = ((CardView) findViewById(R.id.practice));
+        mNormalMode = ((CardView) findViewById(R.id.normal));
+        mOnlineMode = ((CardView) findViewById(R.id.online));
 
-        if (online_mode) {
+        if (boolOnline) {
             SharedPreferences sp = getSharedPreferences("loginUser", 0);
             mProfileName.setText(sp.getString("username_online", getString(R.string.username_random)));
         } else {
@@ -34,5 +41,31 @@ public class SwitchActivity extends AppCompatActivity {
             mProfileName.setText(sp.getString("username_offline", getString(R.string.username_random)));
         }
 
+        mPracticeMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent().setClass(
+                        SwitchActivity.this, GameActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        mNormalMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent().setClass(
+                        SwitchActivity.this, GameActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        mOnlineMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent().setClass(
+                        SwitchActivity.this, GameActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }

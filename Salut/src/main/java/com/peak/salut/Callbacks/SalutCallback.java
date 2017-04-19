9b13ba219d7e047b4a63e1a2bc2f1561f2827719
nian0114 <268078545@qq.com>
2017-04-20 00:27:40 +0800

    新增 Salut库，准备制作Wi-Fi联机

---
+++ Salut/src/main/java/com/peak/salut/Callbacks/SalutCallback.java
@@ -0,0 +1,8 @@
+package com.peak.salut.Callbacks;
+
+/**
+ * Created by markrjr on 2/4/15.
+ */
+public interface SalutCallback {
+    void call();
+}
---
+++ Salut/src/main/java/com/peak/salut/Callbacks/SalutDeviceCallback.java
@@ -0,0 +1,10 @@
+package com.peak.salut.Callbacks;
+
+import com.peak.salut.SalutDevice;
+
+/**
+ * Created by Mark on 4/5/2015.
+ */
+public interface SalutDeviceCallback {
+    void call(SalutDevice device);
+}

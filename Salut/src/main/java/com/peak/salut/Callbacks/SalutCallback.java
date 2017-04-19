ef471278d302fedb719640cb7cf9f834ef190f3d
nian0114 <268078545@qq.com>
2017-04-20 01:33:10 +0800

    Revert "新增 Salut库，准备制作Wi-Fi联机"
    This reverts commit 9b13ba219d7e047b4a63e1a2bc2f1561f2827719.

--- Salut/src/main/java/com/peak/salut/Callbacks/SalutCallback.java
+++
@@ -1,8 +0,0 @@
-package com.peak.salut.Callbacks;
-
-/**
- * Created by markrjr on 2/4/15.
- */
-public interface SalutCallback {
-    void call();
-}
--- Salut/src/main/java/com/peak/salut/Callbacks/SalutDataCallback.java
+++
@@ -1,7 +0,0 @@
-package com.peak.salut.Callbacks;
-
-
-public interface SalutDataCallback {
-
-    void onDataReceived(Object data);
-}
